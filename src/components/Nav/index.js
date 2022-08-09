229023fa1b93d6a74cee667a98a8b85fd9b8e6f9
Tydomx <michaelthai16@gmail.com>
2022-08-09 17:53:20 -0500

    Added ability for gallery pages to be rendered conditionally

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-	const categories = [
-		{ name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-		{ name: 'portraits', description: 'Portraits of people in my life' },
-		{ name: 'food', description: 'Delicious delicacies' },
-		{ name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-	];
+function Nav(props) {
+	const {
+		categories = [],
+		setCurrentCategory,
+		currentCategory
+	} = props;
 
-	function categorySelected(name) {
-		console.log(`${name} clicked`)
-	}
-
-	const handleClick = () => {
-		console.log('click handled')
-	}
+	useEffect(() => {
+		document.title = capitalizeFirstLetter(currentCategory.name);
+		// directs hook to re-render component on changes to value of this state
+		// if currentCategory changes now, component will re-render so that the change in document.title will be visible to the user
+	}, [currentCategory]);
 
 	return (
-		<header>
+		<header className='flex-row px-1'>
 			<h2>
 				<a data-testid="link" href='/'>
-					<span role='img' aria-label='camera'>📸</span> Oh Snap!
+					<span role='img' aria-label='camera'>📸</span>Oh Snap!
 				</a>
 			</h2>
 			<nav>
 				<ul className='flex-row'>
 					<li className='mx-2'>
-						<a data-testid="about" href='#about' onClick={() => handleClick()}>
+						<a data-testid="about" href='#about'>
 							About me
 						</a>
 					</li>
 					<li>
-						<span onClick={() => handleClick()}>Contact</span>
+						<span>Contact</span>
 					</li>
 					{categories.map((category) => {
-						<li className='mx-1' key={category.name} >
-							<span onClick={() => categorySelected(category.name)}>
+						<li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
+							}`}
+							key={category.name}
+						>
+							<span onClick={() => {
+								setCurrentCategory(category.name)
+							}}
+							>
 								{capitalizeFirstLetter(category.name)}
 							</span>
 						</li>
